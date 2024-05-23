8e3f297a9684559bf3c4123072980d094a5d1334
eapldev <eduardoaparecido.dev@gmail.com>
2024-05-22 21:06:43 -0300

    Create cypress project

---
+++ cypress/integration/CAC-TAT.spec.js
@@ -0,0 +1,178 @@
+// CAC-TAT.spec.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+
+//  <reference types="Cypress" />
+
+describe("Central de Atendimento ao Cliente TAT", function () {
+  beforeEach(function () {
+    cy.visit("./src/index.html");
+  });
+
+  it("verifica o título da aplicação", function () {
+    cy.title().should("be.equal", "Central de Atendimento ao Cliente TAT");
+  });
+
+  //   teste 1
+  it("preenche os campos obrigatórios e envia o formulário", function () {
+    const longText =
+      "Teste, teste, teste,teste, teste,teste, teste,teste, teste,teste, teste,teste, teste,teste, teste,teste, teste,teste, teste,teste, teste,";
+
+    cy.get("#firstName").type("Eduardo");
+    cy.get("#lastName").type("Aparecido");
+    cy.get("#email").type("eduardodev@teste.com");
+    cy.get("#open-text-area").type(longText, { delay: 0 });
+    cy.get('button[type="submit"]').click();
+
+    cy.get(".success").should("be.visible");
+  });
+
+  //   teste 2
+  it("exibe mensagem de erro ao submeter o formulário com um email com formatação inválida", function () {
+    cy.get("#firstName").type("Eduardo");
+    cy.get("#lastName").type("Aparecido");
+    cy.get("#email").type("eduardodev@teste,com");
+    cy.get("#open-text-area").type("Teste");
+    cy.get('button[type="submit"]').click();
+
+    cy.get(".error").should("be.visible");
+  });
+
+  //   teste 3
+  it("campo telefone continua vazio quando preenchido com o valor não-numérico", function () {
+    cy.get("#phone").type("abcedefws").should("have.value", "");
+  });
+
+  //   teste 4
+  it("exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário", function () {
+    cy.get("#firstName").type("Eduardo");
+    cy.get("#lastName").type("Aparecido");
+    cy.get("#email").type("eduardodev@teste.com");
+    cy.get("#phone-checkbox").check();
+    cy.get("#open-text-area").type("teste");
+    cy.get('button[type="submit"]').click();
+
+    cy.get(".error").should("be.visible");
+  });
+
+  //   teste 5
+  it("preenche e limpa os campos nome, sobrenome, email e telefone", function () {
+    cy.get("#firstName")
+      .type("Eduardo")
+      .should("have.value", "Eduardo")
+      .clear()
+      .should("have.value", "");
+    cy.get("#lastName")
+      .type("Aparecido")
+      .should("have.value", "Aparecido")
+      .clear()
+      .should("have.value", "");
+    cy.get("#email")
+      .type("eduardodev@teste.com")
+      .should("have.value", "eduardodev@teste.com")
+      .clear()
+      .should("have.value", "");
+    cy.get("#phone")
+      .type("1234567890")
+      .should("have.value", "1234567890")
+      .clear()
+      .should("have.value", "");
+  });
+
+  //   teste 6
+  it("exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios", function () {
+    cy.get('button[type="submit"]').click();
+
+    cy.get(".error").should("be.visible");
+  });
+
+  //   teste 7
+  it("envia o formuário com sucesso usando um comando customizado", function () {
+    cy.fillMandatoryFieldsAndSubmit();
+
+    cy.get(".success").should("be.visible");
+  });
+
+  //   Seção 4
+  it("seleciona um produto (YouTube) por seu texto", function () {
+    cy.get("#product").select("YouTube").should("have.value", "youtube");
+  });
+
+  it("seleciona um produto (Mentoria) por seu valor (value)", function () {
+    cy.get("#product").select("mentoria").should("have.value", "mentoria");
+  });
+
+  it("seleciona um produto (Blog) por seu índice", function () {
+    cy.get("#product").select(1).should("have.value", "blog");
+  });
+
+  //   Seção 5
+  it('marca o tipo de atendimento "Feedback"', function () {
+    cy.get('input[type="radio"][value="feedback"]')
+      .check()
+      .should("have.value", "feedback");
+  });
+
+  it("marca cada tipo de atendimento", function () {
+    cy.get('input[type="radio"]')
+      .should("have.length", 3)
+      .each(function ($radio) {
+        cy.wrap($radio).check();
+        cy.wrap($radio).should("be.checked");
+      });
+  });
+
+  // Seção 6
+  it("marca ambos checkboxes, depois desmarca o último", function () {
+    cy.get('input[type="checkbox"]')
+      .check()
+      .should("be.checked")
+      .last()
+      .uncheck()
+      .should("not.be.checked");
+  });
+
+  // Seção 7
+  it("seleciona um arquivo da pasta fixtures", function () {
+    cy.get('input[type="file"]#file-upload')
+      .should("not.have.value")
+      .selectFile("./cypress/fixtures/example.json")
+      .should(function ($input) {
+        expect($input[0].files[0].name).to.equal("example.json");
+      });
+  });
+
+  it("seleciona um arquivo simulando um drag-and-drop", function () {
+    cy.get('input[type="file"]#file-upload')
+      .should("not.have.value")
+      .selectFile("./cypress/fixtures/example.json", { action: "drag-drop" })
+      .should(function ($input) {
+        expect($input[0].files[0].name).to.equal("example.json");
+      });
+  });
+
+  it("seleciona um arquivo utilizando uma fixture para a qual foi dada um alias", function () {
+    cy.fixture("example.json").as("sampleFile");
+    cy.get('input[type="file"]#file-upload')
+      .selectFile("@sampleFile")
+      .should(function ($input) {
+        expect($input[0].files[0].name).to.equal("example.json");
+      });
+  });
+
+  // Seção 8
+  it("verifica que a política de privacidade abre em outra aba sem a necessidade de um clique", function () {
+    cy.get("#privacy a").should("have.attr", "target", "_blank");
+  });
+
+  it("acessa a página da política de privacidade removendo o target e então clicando no link", function () {
+    cy.get("#privacy a")
+      .invoke("removeAttr", "target")
+      .click();
+    cy.contains("Talking About Testing").should("be.visible");
+  });
+
+});
